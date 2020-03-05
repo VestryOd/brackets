f1091f09039c6863ac1494d6bd3f2357c9f861f4
Maksym Kaspriv <vestry.odessa@gmail.com>
2020-03-05 19:02:36 +0200

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length === 0 || bracketsConfig === null || bracketsConfig === undefined) throw new Error();
+
+  if (str.length % 2 !== 0) return false;
+
+  let arr = [];
+
+  for (const elem of str){
+    if (elem !== arr[arr.length - 1]) {
+      let config = bracketsConfig.find(el => el[0] === elem);
+      if (config === undefined) return false;
+      arr.push(config[1]);
+    } else {
+      arr.pop();
+    }
+  }
+   
+   return arr.length === 0 ? true : false;
 }
